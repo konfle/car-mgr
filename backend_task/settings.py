da4c098d2ab03797d48d983eed543b01e28ec413
konfle <konrad.flejter@gmail.com>
2021-07-04 16:22:20 +0200

    changed settings.py

--- backend_task/settings.py
+++ backend_task/settings.py
@@ -125,8 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = ['static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_app')
+# STATICFILES_DIRS = ['static']
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'all_cars'
