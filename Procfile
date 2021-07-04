9f404db3cf9314409953b6a90103b4c9ea86f079
konfle <konrad.flejter@gmail.com>
2021-07-04 16:11:38 +0200

    deploy all v1

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn backend_task.wsgi --log-file -
--- backend_task/urls.py
+++ backend_task/urls.py
@@ -18,7 +18,6 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('cars_mgmt.urls')),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
