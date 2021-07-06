c4f15df46d9141957e6f635e83f2a3c51302a5b0
konfle <konrad.flejter@gmail.com>
2021-07-06 22:36:40 +0200

    added choices for brand and fuel_type fields

--- cars_mgmt/models.py
+++ cars_mgmt/models.py
@@ -2,10 +2,58 @@ from django.db import models
 
 
 class Car(models.Model):
-    brand = models.CharField(max_length=20, blank=True, null=True)
+
+    class CarBrands(models.TextChoices):
+        ALFA_ROMEO = 'Alfa Romeo'
+        AUDI = 'Audi'
+        BMW = 'BMW'
+        CHEVROLET = 'Chevrolet'
+        CHRYSLET = 'Chrysler'
+        CITROEN = 'Citroen'
+        DACIA = 'Dacia'
+        FIAT = 'Fiat'
+        FORD = 'Ford'
+        HONDA = 'Honda'
+        HYUNDAI = 'Hyundai'
+        INFINITI = 'Infiniti'
+        JAGUAR = 'Jaguar'
+        JEEP = 'Jeep'
+        KIA = 'Kia'
+        LANCIA = 'Lancia'
+        LAND_ROVER = 'Land Rover'
+        LEXUS = 'Lexus'
+        MINI = 'Mini'
+        MAZDA = 'Mazda'
+        MERCEDES_BENZ = 'Mercedes Benz'
+        MITSHUBISHI = 'Mitsubishi'
+        NISSAN = 'Nissan'
+        OPEL = 'Opel'
+        PEUGEOT = 'Peugeot'
+        PORSCHE = 'Porsche'
+        RENAULT = 'Renault'
+        SEAT = 'Seat'
+        SAAB = 'Saab'
+        SKODA = 'Skoda'
+        SSANGYONG = 'Ssangyong'
+        SUBARU = 'Subaru'
+        SUZUKI = 'Suziki'
+        TOYOTA = 'Toyota'
+        VOLKSWAGEN = 'Volkswagen'
+        VOLVO = 'Volvo'
+
+    brand = models.CharField(max_length=20, blank=True, null=True, choices=CarBrands.choices)
     model = models.CharField(max_length=20, blank=True, null=True)
     year_of_production = models.SmallIntegerField(default=2000, blank=True)
-    fuel_type = models.CharField(max_length=10, blank=True, null=True)
+
+    class CarFuelType(models.TextChoices):
+        BENZINE = 'Benzine'
+        BIOFUELS = 'Biofuels'
+        DIESEL = 'Diesel'
+        ELECTRIC = 'Electric'
+        HYBRID = 'Hybrid'
+        LPG = 'LPG'
+
+    fuel_type = models.CharField(max_length=10, blank=True, null=True, choices=CarFuelType.choices)
     mileage = models.DecimalField(default=0, max_digits=8, decimal_places=1, blank=True, null=True)
     registration_number = models.CharField(max_length=7, blank=True, null=True)
 
