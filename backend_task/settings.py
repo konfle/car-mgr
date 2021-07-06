3c3bfd535c6d28c156b9e0a969a965842d80bfb4
konfle <konrad.flejter@gmail.com>
2021-07-06 20:19:17 +0200

    edited stteings

--- backend_task/settings.py
+++ backend_task/settings.py
@@ -126,10 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '../cars_mgmt/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #STATIC_ROOT = os.path.join(BASE_DIR, '../cars_mgmt/static')
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = ['static']
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_DIRS = ['static']
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'all_cars'
