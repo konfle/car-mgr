8d550285b93a4f2630fc02d56d798293ce2ead7b
konfle <konrad.flejter@gmail.com>
2021-07-06 19:11:45 +0200

    clean wsgi.py

--- backend_task/wsgi.py
+++ backend_task/wsgi.py
@@ -10,9 +10,7 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend_task.settings')
 
 application = Cling(get_wsgi_application())
-application = DjangoWhiteNoise(application)
